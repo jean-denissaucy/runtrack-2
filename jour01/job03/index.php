04c10766b149a32e2cee95b84697ddf00bb03cb4
jean-denissaucy <jean-denis.saucy@laplateforme.io>
2025-09-16 11:24:55 +0200

    changement comentaire

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -1,15 +1,15 @@
 <?php
 $hello3= "hello LaPlateforme";
 echo $hello3 . "<br>";
-
+//Math
 $val0=10;
-
+//histoire
 $val1=12;
-
+//sport
 $val2=15;
 
 $total= $val0 + $val1 + $val2;
-
+//note total
 echo $total;
 
 ?>
